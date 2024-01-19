976884737860f2dfb5b64b9316f65ef3cdaf5f1b
Noah Kang <no_ahsark@naver.com>
2024-01-19 19:20:02 +0900

    변수 선언 실습 소스코드 추가

---
+++ ch2/src/VarEx1.java
@@ -0,0 +1,11 @@
+public class VarEx1 {
+    // ch2-3 변수란? 변수의 선언과 저장
+    public static void main(String[] args) {
+        // 변수를 선언하고 사용함으로 간단하게 값을 유연하게 변경 가능합니다.
+        int x = 4, y = 2;
+        System.out.println(x+y); // 덧셈
+        System.out.println(x-y); // 뺼셈
+        System.out.println(x*y); // 곱셈
+        System.out.println(x/y); // 나눗셈
+    }
+}
